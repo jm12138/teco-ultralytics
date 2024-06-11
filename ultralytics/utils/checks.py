099f9587bfc392123e1dca2ca132b85e7347c682
jm12138 <2286040843@qq.com>
2024-06-11 16:57:48 +0000

    Adapted to autocast

--- ultralytics/utils/checks.py
+++ ultralytics/utils/checks.py
@@ -648,7 +648,7 @@ def check_amp(model):
     def amp_allclose(m, im):
         """All close FP32 vs AMP results."""
         a = m(im, device=device, verbose=False)[0].boxes.data  # FP32 inference
-        with torch.sdaa.amp.autocast(True):
+        with torch.sdaa.amp.autocast(enabled=True):
             b = m(im, device=device, verbose=False)[0].boxes.data  # AMP inference
         del m
         return a.shape == b.shape and torch.allclose(a, b.float(), atol=0.5)  # close to 0.5 absolute tolerance
